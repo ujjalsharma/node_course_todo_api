5f8acc48e7aafd609e18c09a927c07abc4e54b1d
ujjalsharma <ujjalsharma81@gmail.com>
2020-10-21 23:09:15 +0530

    add something

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-var uri = "mongodb+srv://dbRagnar:ironman19@cluster0.dbykr.mongodb.net/test?retryWrites=true&w=majority";
-mongoose.connect(uri || "mongodb://localhost:27017/TodoApp");
+mongoose.connect(process.env.MONGO_URI || "mongodb://localhost:27017/TodoApp");
 
 
 module.exports = {mongoose};
