62f731f90feee597797803f995d71b90976a7afd
ujjalsharma <ujjalsharma81@gmail.com>
2020-10-23 13:02:51 +0530

    add GET/users/me middleware

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ var {mongoose} = require("./db/mongoose");
 var {Todo} = require("./models/todo");
 var {User} = require("./models/user");
 const { ObjectID } = require("mongodb");
+var {authenticate} = require("./middleware/authenticate");
 
 var app = express();
 var port = process.env.PORT || 3000;
@@ -118,6 +119,11 @@ app.post("/users", (req, res) => {
         res.status(400).send(e);
     });
     
+});
+
+
+app.get("/users/me", authenticate, (req, res) => {
+    res.send(req.user);
 });
 
 
